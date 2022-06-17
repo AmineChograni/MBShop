dd75c1de1754292fe2018cd187ce557ef28d207a
Amine CHOGRANI <aminechograni@gmail.com>
2022-06-17 19:49:38 +0100

    about us beta no style

--- MBShopFE/src/app/static/home/home.module.ts
+++ MBShopFE/src/app/static/home/home.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { ProductSectionComponent } from './product-section/product-section.component';
-
 //ng-zorro
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ProductSectionComponent } from './product-section/product-section.component';
 import { AboutSectionComponent } from './about-section/about-section.component';
+
+
 
 
 @NgModule({
@@ -20,7 +23,8 @@ import { AboutSectionComponent } from './about-section/about-section.component';
     HomeRoutingModule,
     NzGridModule,
     NzCarouselModule,
-    NzCardModule
+    NzCardModule,
+    NzButtonModule
   ]
 })
 export class HomeModule { }
