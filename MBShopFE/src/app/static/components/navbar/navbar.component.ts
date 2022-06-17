ac2ec92784b416678beadc6234258c7f66a06729
Amine CHOGRANI <aminechograni@gmail.com>
2022-06-17 18:20:42 +0100

    navbar beta

--- MBShopFE/src/app/static/components/navbar/navbar.component.ts
+++ MBShopFE/src/app/static/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,28 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+      $(function() {
+        $('nav ul li > a:not(:only-child)').click(function(e) {
+          $(this)
+            .siblings('.nav-dropdown')
+            .slideToggle()
+          $('.nav-dropdown')
+            .not($(this).siblings())
+            .hide()
+          e.stopPropagation()
+        })
+        $('html').click(function() {
+          $('.nav-dropdown').hide()
+        })
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function() {
+          $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function() {
+          this.classList.toggle('active')
+        })
+      })
   }
 
 }
