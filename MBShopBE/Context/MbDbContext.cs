68b036324a470d60f3d3cede56048f9e24eb809e
Amine CHOGRANI <aminechograni@gmail.com>
2022-11-01 16:00:28 +0100

    create DbContext + add ConnectionString + add DbContext service on Startup

---
+++ MBShopBE/Context/MbDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MBShopBE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBShopBE.Context
+{
+    public class MbDbContext:DbContext
+    {
+        public MbDbContext(DbContextOptions<MbDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Commande> Commandes { get; set; }
+        public DbSet<Couleur> Couleurs { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Taille> Tailles { get; set; }
+    }
+}
