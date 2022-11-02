e4554f3ab5dc9872db2ae09321c986e44c33fb2e
Amine CHOGRANI <aminechograni@gmail.com>
2022-11-02 19:05:12 +0100

    create migration

--- MBShopBE/Migrations/20221102180457_ImagesCreate.Designer.cs
+++ MBShopBE/Migrations/20221102180457_ImagesCreate.Designer.cs
@@ -4,14 +4,16 @@ using MBShopBE.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MBShopBE.Migrations
 {
     [DbContext(typeof(MbDbContext))]
-    partial class MbDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221102180457_ImagesCreate")]
+    partial class ImagesCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MBShopBE/Migrations/20221102180457_ImagesCreate.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MBShopBE.Migrations
+{
+    public partial class ImagesCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageMain",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Imageone",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Imagethree",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Imagetwo",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Image_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MImage_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_ProductId",
+                table: "Image",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MImage_ProductId",
+                table: "MImage",
+                column: "ProductId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropTable(
+                name: "MImage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageMain",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Imageone",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Imagethree",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Imagetwo",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
