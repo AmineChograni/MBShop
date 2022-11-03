c1b5e1083470164d5ef6d3a387c57b50d7a8582a
Amine CHOGRANI <aminechograni@gmail.com>
2022-11-03 20:40:08 +0100

    add date to commande model

--- MBShopBE/Models/Commande.cs
+++ MBShopBE/Models/Commande.cs
@@ -1,4 +1,6 @@
-﻿namespace MBShopBE.Models
+﻿using System;
+
+namespace MBShopBE.Models
 {
     public class Commande
     {
@@ -12,5 +14,6 @@
         public string CodePostal { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
+        public DateTime Date { get; set; }
     }
 }
