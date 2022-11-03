45b10cb02849c464aa8c071cf12611e3528a308c
Amine CHOGRANI <aminechograni@gmail.com>
2022-11-03 20:14:40 +0100

    generate commande controller

--- MBShopBE/Controllers/PImageController.cs
+++ MBShopBE/Controllers/PImageController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Net.Http.Headers;
 using System.Linq;
 using System.Threading.Tasks;
+using MBShopBE.Context;
+using MBShopBE.Models;
 
 namespace MBShopBE.Controllers
 {
@@ -12,6 +14,14 @@ namespace MBShopBE.Controllers
     [ApiController]
     public class PImageController : ControllerBase
     {
+        private readonly MbDbContext _context;
+
+        public PImageController(MbDbContext context)
+        {
+            _context = context;
+        }
+
+
         [HttpPost, DisableRequestSizeLimit]
         public async Task<IActionResult> UploadAsync()
         {
@@ -19,6 +29,7 @@ namespace MBShopBE.Controllers
             {
                 var formCollection = await Request.ReadFormAsync();
                 var file = formCollection.Files.First();
+                
 
                 var folderName = Path.Combine("Resources", "ProductImg");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
@@ -31,7 +42,16 @@ namespace MBShopBE.Controllers
                     {
                         file.CopyTo(stream);
                     }
-                    return Ok(new { dbPath });
+
+                    var pimage = new MImage();
+                    pimage.ImageURL = dbPath;
+                    var dict = Request.Form.ToDictionary(x => x.Key, x => x.Value.ToString());
+                    pimage.ProductId = Int32.Parse(dict["ProductId"]);
+
+                    _context.MImages.Add(pimage);
+                    await _context.SaveChangesAsync();
+                    CreatedAtAction("GetMImage", new { id = pimage.Id }, pimage);
+                    return Created("test",pimage);
                 }
                 else
                 {
