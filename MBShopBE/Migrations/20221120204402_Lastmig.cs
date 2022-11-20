95e2d77dd924e3d159a66b2033592d0f7110cc8d
Amine CHOGRANI <aminechograni@gmail.com>
2022-11-20 22:01:36 +0100

    add color tag on product images

---
+++ MBShopBE/Migrations/20221120204402_Lastmig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MBShopBE.Migrations
+{
+    public partial class Lastmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "ProdImages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "ProdImages");
+        }
+    }
+}
--- MBShopBE/Models/ProdImage.cs
+++ MBShopBE/Models/ProdImage.cs
@@ -3,6 +3,7 @@
     public class ProdImage
     {
         public int Id { get; set; }
+        public string Color { get; set; }
         public string ImageURL { get; set; }
         public int ProductId { get; set; }
 
