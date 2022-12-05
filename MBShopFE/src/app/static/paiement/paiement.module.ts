16b4fb5b91efaaa01f3c5edda21d75729a968774
Amine CHOGRANI <aminechograni@gmail.com>
2022-12-05 12:28:04 +0100

    commande

--- MBShopFE/src/app/static/paiement/paiement.module.ts
+++ MBShopFE/src/app/static/paiement/paiement.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { PaiementRoutingModule } from './paiement-routing.module';
 import { PaiementComponent } from './paiement.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [PaiementComponent],
   imports: [
     CommonModule,
-    PaiementRoutingModule
+    PaiementRoutingModule,
+    NzIconModule,
+    NzGridModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    HttpClientModule
+
   ]
 })
 export class PaiementModule { }
