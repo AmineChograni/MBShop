ea2ca00dcd9eb80de6625d124429948b5036a7da
Amine CHOGRANI <aminechograni@gmail.com>
2022-12-19 17:57:11 +0100

    create product and Tailles models

---
+++ MBShopFE/src/app/static/models/Product.ts
@@ -0,0 +1,30 @@
+import { Tailles } from "./Tailles";
+
+export class Product {
+    id?: Number;
+    label: String;
+    description: String;
+    sold: Boolean;
+    new: Boolean;
+    price: Number;
+    priceSold: Number;
+    categoryId: Number;
+    tailles:Tailles[];
+    couleurs:Couleurs[];
+    tags:Tags[];
+    imageURL: String;
+    prodImages:ProdImages[];
+    date: Date;
+
+    constructor(id: Number, label: String, description: String, sold: Boolean, neww: boolean ,price: Number, priceSold: Number, categoryId: Number, imageURL: String){
+        this.id=id;
+        this.label=label;
+        this.description=description;
+        this.sold=sold;
+        this.new=neww;
+        this.price=price;
+        this.priceSold=priceSold;
+        this.categoryId=categoryId;
+        this.imageURL=imageURL;
+    }
+}
---
+++ MBShopFE/src/app/static/models/Tailles.ts
@@ -0,0 +1,12 @@
+export class Tailles{
+    id?:Number;
+    label: String;
+    productId: Number;
+
+    constructor(id: Number, label: String, productId: Number){
+        this.id=id;
+        this.label=label;
+        this.productId
+
+    }
+}
